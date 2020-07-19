e5823a5f74fc9cfd90717a3f2054dc3b3825c2e6
spaceark7 <spacearkchief@gmail.com>
2020-07-19 21:16:16 +0700

    Updates

---
+++ precache-manifest.9b721eacc7710bfddf36ccd69b8058ff.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b411b198286f65fbfce14df314af42",
+    "url": "/ami/index.html"
+  },
+  {
+    "revision": "bf51efc667e36ed4d4d5",
+    "url": "/ami/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "83587c626db27fbf5697",
+    "url": "/ami/static/js/2.f211f276.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ami/static/js/2.f211f276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf51efc667e36ed4d4d5",
+    "url": "/ami/static/js/main.542f88fd.chunk.js"
+  },
+  {
+    "revision": "8944c9f6c5f31bad450c",
+    "url": "/ami/static/js/runtime-main.2c5f0e04.js"
+  },
+  {
+    "revision": "fb477057c7eb857f6061a064bcc730c0",
+    "url": "/ami/static/media/A4.fb477057.jpg"
+  },
+  {
+    "revision": "87c374c194452ead26214c054b3fc395",
+    "url": "/ami/static/media/a1.87c374c1.jpg"
+  },
+  {
+    "revision": "cde390d3949de975bc43adcf28982fdf",
+    "url": "/ami/static/media/a2.cde390d3.jpg"
+  },
+  {
+    "revision": "552630657337c46ca1d238dcbaa09183",
+    "url": "/ami/static/media/a3.55263065.jpg"
+  },
+  {
+    "revision": "d021b28391bc800b9dbc71ef6ba5813c",
+    "url": "/ami/static/media/backsound.d021b283.mp3"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ami/static/media/logo.5d5d9eef.svg"
+  }
+]);
